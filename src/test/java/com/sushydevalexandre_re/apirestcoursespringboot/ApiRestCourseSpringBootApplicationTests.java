98ecdb9e7b3ed580ac625a2e50356661690f7dcb
Nathan CHATEAU <n.chateau.pro@gmail.com>
2024-03-04 15:24:20 +0400

    Inital commit

---
+++ src/test/java/com/sushydevalexandre_re/apirestcoursespringboot/ApiRestCourseSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sushydevalexandre_re.apirestcoursespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestCourseSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
